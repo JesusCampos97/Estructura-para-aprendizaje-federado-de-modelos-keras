77664c6a40fc43bcd465e82c37feaa632d2689c5
Raspberry <jesus.campos302@alu.uhu.es>
2022-05-04 18:16:58 +0200

    hot fix

---
+++ pruebas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+
+with open('/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/2/04-05-2022 14-00/d0/history.json', 'r') as f:
+	history = json.loads(f.read())
+#extract an element in the response
+print(history[0])
+last_acc=history[1]["accuracy"]
+print("last accuracy: "+str(last_acc))
