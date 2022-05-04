81e342bb15bdbe0d913d6baa2c9779555b3e7c2e
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-04 18:18:10 +0200

    hot fix

--- pruebas.py
+++ pruebas.py
@@ -4,6 +4,6 @@ import json
 with open('/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/2/04-05-2022 14-00/d0/history.json', 'r') as f:
 	history = json.loads(f.read())
 #extract an element in the response
-print(history[0])
+print(history[1])
 last_acc=history[1]["accuracy"]
 print("last accuracy: "+str(last_acc))
