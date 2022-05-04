0de37d5f1f2ff5f337580db700f813c2e882d591
Raspberry <jesus.campos302@alu.uhu.es>
2022-05-04 22:30:54 +0200

    prueba

--- pruebas.py
+++ pruebas.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import json
+import os
 
-with open('/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/2/04-05-2022 14-00/d0/history.json', 'r') as f:
+"""with open('/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/2/04-05-2022 14-00/d0/history.json', 'r') as f:
 	history = json.loads(f.read())
 #extract an element in the response
-print(history[1])
+print(history)
 last_acc=history[1]["accuracy"]
-print("last accuracy: "+str(last_acc))
+print("last accuracy: "+str(last_acc))"""
+num_devices=0
+list_files=os.listdir("/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/2/04-05-2022 19-41")
+for i in list_files:
+	if i.startswith("d"):
+		num_devices+=1
+
+print(num_devices)
