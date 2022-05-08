865af354f019855f14cf82e021a303acacfab81e
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-08 20:55:24 +0200

    hot fix

--- plot_dataframes.py
+++ plot_dataframes.py
@@ -13,10 +13,11 @@ df = pd.read_csv(path+"/results.csv")
 df_aux_d0=df[(df.device==0)]
 print(df_aux_d0.head())
 df_aux_d1=df[(df.device==1)]
-print(df_aux_d0.head())
+print(df_aux_d1.head())
 
 #df_aux_d0.plot(x='accuracy', kind='line')
-
+print(df_aux_d0['accuracy'])
+print(df_aux_d0['accuracy'][0])
 
 plt.plot(df_aux_d0['accuracy'])
 plt.plot(df_aux_d1['accuracy'])
