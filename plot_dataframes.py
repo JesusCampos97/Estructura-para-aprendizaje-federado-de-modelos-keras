636cb3f78633d555e1129676704ab79a3c07bcaf
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-09 18:41:03 +0200

    hot fix

--- plot_dataframes.py
+++ plot_dataframes.py
@@ -14,15 +14,22 @@ df_aux_d0=df[(df.device==0)]
 print(df_aux_d0.head())
 df_aux_d1=df[(df.device==1)]
 print(df_aux_d1.head())
+df_aux_d2=df[(df.device==2)]
+df_aux_d3=df[(df.device==3)]
+df_aux_d4=df[(df.device==4)]
+
 
 #df_aux_d0.plot(x='accuracy', kind='line')
-print(df_aux_d0['accuracy'])
-print(df_aux_d0['accuracy'][0])
+print(df_aux_d0['val_accuracy'])
+print(df_aux_d0['val_accuracy'][0])
 
-plt.plot(df_aux_d0['accuracy'])
-plt.plot(df_aux_d1['accuracy'])
+plt.plot(df_aux_d0['val_accuracy'])
+plt.plot(df_aux_d1['val_accuracy'])
+plt.plot(df_aux_d2['val_accuracy'])
+plt.plot(df_aux_d3['val_accuracy'])
+plt.plot(df_aux_d4['val_accuracy'])
 plt.title('Accuracy comparation')
 plt.ylabel('accuracy')
 plt.xlabel('days')
-plt.legend(['d0', 'd1'], loc='upper left')
+plt.legend(['d0', 'd1', 'd2', 'd3', 'd4'], loc='upper left')
 plt.savefig(path+'/pruebas.png')
