d335728bffe6734bdb8ac9e98eb9e849d875735f
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-12 18:25:51 +0200

    hot fix

--- plot_dataframes.py
+++ plot_dataframes.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/5/12-05-2022 10-15"
+path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/5/12-05-2022 15-28"
 df = pd.read_csv(path+"/results.csv")  
 
 df_aux_acc_mean=df.groupby(['day'])['accuracy'].mean()
