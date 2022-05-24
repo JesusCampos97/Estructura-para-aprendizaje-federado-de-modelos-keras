88421e2f4d94b610165f7a82dd59d689d88bc362
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-24 10:15:32 +0200

    hot fix

--- executeOnlyServer.py
+++ executeOnlyServer.py
@@ -2,5 +2,5 @@ from datetime import datetime
 from Server import Server
 from Devices import Device
 
-server = Server()
+server = Server(3)
 server.merge("/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/5/24-05-2022 09-15")
