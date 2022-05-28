6ed80eec99767d8785472d2ce58c70ad4487ee55
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-28 11:14:18 +0200

    hot fix

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -44,20 +44,20 @@ def processImages(path_dataset):
 if __name__ == "__main__":
 
 
-    num_devices=10 # se ha hehco con 5, quedan 10 y 20
+    num_devices=1 # se ha hehco con 5, quedan 10 y 20
     data_percentage=0.8
     train_percentage=0.8
     path_devices="./Devices/"#"Devices/5/20042022 (2)"
     path_dataset="/datasets" #path donde se encuentra el dataset descomprimido
-    model_type=4 #Se debera de pasar por parametros
-    epochs=1 #Se debera de pasar por parametros
+    model_type=1 #Se debera de pasar por parametros
+    epochs=5 #Se debera de pasar por parametros
     image_height = 256 #224
     image_width = 256 #224
     batch_size = 32
     steps_per_epoch = 10
     dataset_rename = False
     #num_etapas=5 #Serian X días distintos, donde se seguiria ejecutando el federado, osea 2 dispositivos, entrenan, mergean y evaluan, se quedan el mejor y lo vuelven a evlauar todo con el nuevo modelo
-    num_etapas_list=[15] #[5,10,15]
+    num_etapas_list=[1] #[5,10,15]
     merge_type=1 #1-FederatedAverage, 2-exponentially decreasing weights, 3- PonderedFederatedAverage
 
     #Creo las carpetas de los datasets y los renombro
