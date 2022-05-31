a33277b069234a275175764ffeb0b6de2df3bbb4
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-31 13:55:03 +0200

    hot fix

--- inferenceImageByCoral.py
+++ inferenceImageByCoral.py
@@ -9,9 +9,35 @@ import tensorflow as tf
 path="./Devices/5/30-05-2022 12-40/"
 
 h5_path=path+"model_merged.h5"
-converter = tf.lite.TFLiteConverter.from_keras_model(h5_path)
-tfmodel = converter.convert()
-open(path+"model.tflite" , "wb").write(tfmodel)
+
+import sys, os
+import traceback
+
+from os.path                    import splitext, basename
+
+print(tf.__version__)
+
+mod_path = "data/lp-detector/wpod-net_update1.h5"
+
+def load_model(path,custom_objects={},verbose=0):
+    #from tf.keras.models import model_from_json
+
+    path = splitext(path)[0]
+    with open('%s.json' % path,'r') as json_file:
+        model_json = json_file.read()
+    model = tf.keras.models.model_from_json(model_json, custom_objects=custom_objects)
+    model.load_weights('%s.h5' % path)
+    if verbose: print('Loaded from %s' % path)
+    return model
+
+keras_mod = load_model(h5_path)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(keras_mod)
+tflite_model = converter.convert()
+
+# Save the TF Lite model.
+with tf.io.gfile.GFile(path+"model.tflite", 'wb') as f:
+    f.write(tflite_model)
 
 
 
