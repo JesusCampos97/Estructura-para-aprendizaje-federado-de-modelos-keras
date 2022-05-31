0047d304c7ebc9ce5118193ebdf662bae92d1691
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-31 15:12:15 +0200

    hot fix

--- inferenceImageByCoral.py
+++ inferenceImageByCoral.py
@@ -100,3 +100,28 @@ new_image = load_image(img_path)
 # check prediction
 pred = model.predict(new_image)
 print(pred)
+
+
+# Specify the TensorFlow model, labels, and image
+script_dir = pathlib.Path(__file__).parent.absolute()
+model_file = os.path.join(script_dir, path+"model.tflite")
+label_file = os.path.join(script_dir, path+'labels.txt')
+image_file = os.path.join(script_dir,'/datasets/dataset positivo/crosswalk_400.jpg')
+
+# Initialize the TF interpreter
+interpreter = edgetpu.make_interpreter(model_file)
+interpreter.allocate_tensors()
+
+# Resize the image
+size = common.input_size(interpreter)
+image = new_image#Image.open(image_file).convert('RGB').resize(size, Image.ANTIALIAS)
+
+# Run an inference
+common.set_input(interpreter, image)
+interpreter.invoke()
+classes = classify.get_classes(interpreter, top_k=1)
+
+# Print the result
+labels = dataset.read_label_file(label_file)
+for c in classes:
+  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
