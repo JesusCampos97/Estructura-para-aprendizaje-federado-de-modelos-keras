289582016d82cdf0ffeb8bd7759cd3a40bc0026d
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-05-31 15:28:02 +0200

    pruebas coral

--- inferenceImageByCoral.py
+++ inferenceImageByCoral.py
@@ -1,3 +1,4 @@
+from operator import ne
 import os
 import pathlib
 from pycoral.utils import edgetpu
@@ -18,41 +19,6 @@ import sys, os
 import traceback
 
 from os.path                    import splitext, basename
-
-print(tf.__version__)
-
-
-#model = tf.keras.models.load_model(h5_path)
-#converter = tf.lite.TFLiteConverter.from_keras_model(model)
-#tflite_model = converter.convert()
-#open(path+"model.tflite", "wb").write(tflite_model)
-
-
-
-# Specify the TensorFlow model, labels, and image
-script_dir = pathlib.Path(__file__).parent.absolute()
-model_file = os.path.join(script_dir, path+"model.tflite")
-label_file = os.path.join(script_dir, path+'labels.txt')
-img_path = '/datasets/dataset negativo/road_400.jpg'    # dog
-
-# Initialize the TF interpreter
-interpreter = edgetpu.make_interpreter(model_file)
-interpreter.allocate_tensors()
-
-# Resize the image
-size = common.input_size(interpreter)
-image = Image.open(img_path).convert('RGB').resize(size, Image.ANTIALIAS)
-
-# Run an inference
-common.set_input(interpreter, image)
-interpreter.invoke()
-classes = classify.get_classes(interpreter, top_k=1)
-
-# Print the result
-labels = dataset.read_label_file(label_file)
-for c in classes:
-  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
-
 
 
 # load_model_sample.py
@@ -75,25 +41,24 @@ def load_image(img_path, show=False):
     return img_tensor
 
 # load model
-model = load_model(h5_path)
+#model = load_model(h5_path)
 
 # image path
-img_path = '/datasets/dataset negativo/road_400.jpg'    # dog
-#img_path = '/home/pi/Downloads/crosswalk'      # cat
+img_path = '/home/pi/Downloads/crosswalk'    # dog
+img_path2 = '/home/pi/Downloads/road'      # cat
 
 # load a single image
+images_list=[]
 new_image = load_image(img_path)
+new_image2 = load_image(img_path2)
 
-# check prediction
-pred = model.predict(new_image)
-print(pred)
-
+images_list.append(new_image)
+images_list.append(new_image2)
 
 # Specify the TensorFlow model, labels, and image
 script_dir = pathlib.Path(__file__).parent.absolute()
 model_file = os.path.join(script_dir, path+"model.tflite")
 label_file = os.path.join(script_dir, path+'labels.txt')
-image_file = os.path.join(script_dir,'/datasets/dataset negativo/road_400.jpg')
 
 # Initialize the TF interpreter
 interpreter = edgetpu.make_interpreter(model_file)
@@ -101,14 +66,16 @@ interpreter.allocate_tensors()
 
 # Resize the image
 size = common.input_size(interpreter)
-image = new_image#Image.open(image_file).convert('RGB').resize(size, Image.ANTIALIAS)
+image = new_image
 
 # Run an inference
-common.set_input(interpreter, image)
-interpreter.invoke()
-classes = classify.get_classes(interpreter, top_k=1)
+for image in images_list:
+    common.set_input(interpreter, image)
+    interpreter.invoke()
+    classes = classify.get_classes(interpreter, top_k=1)
 
-# Print the result
-labels = dataset.read_label_file(label_file)
-for c in classes:
-  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+    # Print the result
+    labels = dataset.read_label_file(label_file)
+    for c in classes:
+        print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+
