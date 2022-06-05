bcaa0a382780499de799e8681af88593411867b0
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-05 13:05:02 +0200

    hot fix

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -44,7 +44,7 @@ def processImages(path_dataset):
 if __name__ == "__main__":
 
 
-    num_devices=20 # se ha hehco con 5, quedan 10 y 20
+    num_devices=5 # se ha hehco con 5, quedan 10 y 20
     data_percentage=0.8
     train_percentage=0.8
     path_devices="./Devices/"#"Devices/5/20042022 (2)"
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     dataset_rename = False
     #num_etapas=5 #Serian X días distintos, donde se seguiria ejecutando el federado, osea 2 dispositivos, entrenan, mergean y evaluan, se quedan el mejor y lo vuelven a evlauar todo con el nuevo modelo
     num_etapas_list=[15] #[5,10,15]
-    merge_type=1 #1-FederatedAverage, 2-exponentially decreasing weights, 3- PonderedFederatedAverage
+    merge_type=3 #1-FederatedAverage, 2-exponentially decreasing weights, 3- PonderedFederatedAverage
 
     #Creo las carpetas de los datasets y los renombro
     start_dataset_renames = time.time()
