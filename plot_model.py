a333372c3468d5507ae2b4edd77dd1e18f69034f
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-06 19:56:12 +0200

    plot model

--- plot_model.py
+++ plot_model.py
@@ -4,3 +4,4 @@ from tensorflow.keras.utils import plot_model
 path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/5/05-06-2022 13-09"
 model=tf.keras.models.load_model(path+'/model_merged.h5')
 plot_model(model, to_file=path+'/model_plot.png', show_shapes=True, show_layer_names=True)
+model.summary()
