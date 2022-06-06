c4a4567679c7daaa36d7609de2ce69ef51241d66
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-06 19:31:17 +0200

    plot model

--- plot_model.py
+++ plot_model.py
@@ -1,7 +1,5 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.utils.vis_utils import plot_model
 import tensorflow as tf
+from tensorflow.keras.utils.vis_utils import plot_model
 
 path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/5/05-06-2022 13-09"
 model=tf.keras.models.load_model(path+'/model_merged.h5')
