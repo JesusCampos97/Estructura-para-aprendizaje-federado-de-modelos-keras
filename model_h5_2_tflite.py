7ab6da8f7113bed322d0768422b82c81012342db
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-12 13:35:02 +0200

    h5 to tflite

--- model_h5_2_tflite.py
+++ model_h5_2_tflite.py
@@ -1,7 +1,8 @@
-from tensorflow.contrib import lite
+import tensorflow as tf
 
 path_model_h5="./Devices/20/02-06-2022 09-09/model_merged.h5"
 
-converter = lite.TFLiteConverter.from_keras_model_file( path_model_h5)
-tfmodel = converter.convert()
-open ("./Devices/20/02-06-2022 09-09/model.tflite" , "wb").write(tfmodel)
+model = tf.keras.models.load_model(path_model_h5)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+open("./Devices/20/02-06-2022 09-09/model.tflite", "wb").write(tflite_model)
