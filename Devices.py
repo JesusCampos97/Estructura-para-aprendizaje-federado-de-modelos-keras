9cf14784254024381672f883b0d2dcb4158aecb8
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-19 13:58:32 +0200

    hot fix

--- Devices.py
+++ Devices.py
@@ -95,12 +95,17 @@ class Device:
         if history.history['val_accuracy'][0]>last_val_acc or (history.history['val_accuracy'][0]==last_val_acc and history.history['accuracy'][0]>last_acc):
             #si mi entrenamiento es mejor entonces lo cambio, si no me quedo con mi modelo anterior que iba mejor
             model.save(self.path+"/model.h5", overwrite=True)
-        #print("modelo guardado")
-        self.plotHistory(history)
-        self.saveConfig(history)
-        #self.deleteTempFiles()
-        return history.history['accuracy'][0], history.history['val_accuracy'][0], history.history['loss'][0], history.history['val_loss'][0]
+            #print("modelo guardado")
+            self.plotHistory(history)
+            self.saveConfig(history)
+            #self.deleteTempFiles()
+            return history.history['accuracy'][0], history.history['val_accuracy'][0], history.history['loss'][0], history.history['val_loss'][0]
 
+        else:
+            #me quedo con mi modelo anterior y por lo tanto con su validacion y acc etc
+            return history_last[-1]["accuracy"], history_last[-1]["val_accuracy"], history_last[-1]["loss"], history_last[-1]["val_loss"]
+
+        
 
     def loadDataImages(self):
         #Aqui hay que cmabiar al forma de trabajar. La clase 0 tiene que ser siempre la misma... si no cascará al mergear 2 modelos siempre.... es decir no irá bien
