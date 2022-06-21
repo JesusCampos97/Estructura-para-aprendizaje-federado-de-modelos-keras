311ab163f3341eebbd2aa52419176eac3300efa0
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-21 09:13:05 +0200

    hot fix

--- plot_dataframes.py
+++ plot_dataframes.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/20/18-06-2022 10-59"
+path="/home/pi/Desktop/proyecto/Estructura-para-aprendizaje-federado-de-modelos-keras/Devices/20/19-06-2022 23-54"
 df = pd.read_csv(path+"/results.csv")  
 
 df_add_aux = {'device': -1, 'accuracy': 0.8529411554, 'val_accuracy': 0.7568807602,'loss':0.2944834175, 'val_loss':0.4638305307,'day':-1,
