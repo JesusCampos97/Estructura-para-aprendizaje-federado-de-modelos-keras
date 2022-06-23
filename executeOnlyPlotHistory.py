108948022583e37718c30145ace62d6f8ce61133
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-23 09:10:20 +0200

    execute only plot history

--- executeOnlyPlotHistory.py
+++ executeOnlyPlotHistory.py
@@ -7,7 +7,7 @@ with open('./Devices/1/22-06-2022 20-49/d0/history.json', 'r') as f:
     history = json.loads(f.read())
 
 
-df=pd.DataFrame.from_dict(history[0], orient="index")
+df=pd.DataFrame.from_dict(history)
 print(df)
 figure=df.plot(x="Epochs", y="Accuracy")
 figure.legend(['train', 'val'], loc='upper left')
