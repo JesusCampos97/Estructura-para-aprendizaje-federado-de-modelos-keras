44ba1942fde6b570d7d37afb9922ee27d43bcd52
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-23 08:41:07 +0200

    execute only plot history

--- executeOnlyPlotHistory.py
+++ executeOnlyPlotHistory.py
@@ -5,8 +5,8 @@ import json
 with open('./Devices/1/22-06-2022 20-49/d0/history.json', 'r') as f:
     history = json.loads(f.read())
 
-plt.plot(history['accuracy'])
-plt.plot(history['val_accuracy'])
+plt.plot(history['accuracy'][0])
+plt.plot(history['val_accuracy'][0])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
@@ -14,8 +14,8 @@ plt.legend(['train', 'val'], loc='upper left')
 plt.savefig('./Devices/1/22-06-2022 20-49/d0/accuracy.png')
 plt.clf()
 
-plt.plot(history['loss'])
-plt.plot(history['val_loss'])
+plt.plot(history['loss'][0])
+plt.plot(history['val_loss'][0])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
