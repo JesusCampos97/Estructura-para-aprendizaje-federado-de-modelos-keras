65527aa026e48ed0e8ef772290894083d2a57e05
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-23 08:46:20 +0200

    execute only plot history

--- executeOnlyPlotHistory.py
+++ executeOnlyPlotHistory.py
@@ -7,8 +7,8 @@ with open('./Devices/1/22-06-2022 20-49/d0/history.json', 'r') as f:
 
 print(history)
 
-plt.plot(history['accuracy'][0])
-plt.plot(history['val_accuracy'][0])
+plt.plot(history[0]['accuracy'])
+plt.plot(history[0]['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
@@ -16,8 +16,8 @@ plt.legend(['train', 'val'], loc='upper left')
 plt.savefig('./Devices/1/22-06-2022 20-49/d0/accuracy.png')
 plt.clf()
 
-plt.plot(history['loss'][0])
-plt.plot(history['val_loss'][0])
+plt.plot(history[0]['loss'])
+plt.plot(history[0]['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
