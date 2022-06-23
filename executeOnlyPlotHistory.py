b88c28aa20df1be798fec19b9ff8377e70cced39
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-23 09:03:50 +0200

    execute only plot history

--- executeOnlyPlotHistory.py
+++ executeOnlyPlotHistory.py
@@ -1,26 +1,15 @@
 from matplotlib import pyplot as plt
 import json
+import pandas as pd
 
 
 with open('./Devices/1/22-06-2022 20-49/d0/history.json', 'r') as f:
     history = json.loads(f.read())
 
-print(history)
 
-plt.plot(history[0]['accuracy'])
-plt.plot(history[0]['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.savefig('./Devices/1/22-06-2022 20-49/d0/accuracy.png')
-plt.clf()
-
-plt.plot(history[0]['loss'])
-plt.plot(history[0]['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.savefig('./Devices/1/22-06-2022 20-49/d0/loss.png')
-plt.clf()
+df=pd.DataFrame(history[0])
+print(df)
+figure=df.plot(x="Epochs", y="Accuracy")
+figure.legend(['train', 'val'], loc='upper left')
+figure.savefig('./Devices/1/22-06-2022 20-49/d0/accuracy.png')
+figure.title('Model accuracy')
