64df3ff0ef2920b46de8fccd703103ba017d589e
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-26 19:02:02 +0200

    hot fix

--- inferenceVideoStreaming.py
+++ inferenceVideoStreaming.py
@@ -68,11 +68,7 @@ def executeSound():
 
     channela =sound.play()
 
-    while pygame.mixer.get_busy():
-        pygame.time.delay(10)
-        ev = pygame.event.poll() 
-        if ev.type == pygame.QUIT:
-            break
+    pygame.time.delay(10)
     pygame.quit()
     
       
