7475adb7b09506a5a803077e5d4baef546eec6e1
JesusCampos97 <jesus.campos302@alu.uhu.es>
2022-06-26 18:36:57 +0200

    hot fix

--- inferenceVideoStreaming.py
+++ inferenceVideoStreaming.py
@@ -175,7 +175,7 @@ with picamera.PiCamera() as camera:
         start_t3=time.time()
         #preview.set_data(img)
         preview.set_data(stream)
-        fig.canvas.update()
+        fig.canvas.show()
         
         time_elapsed(start_t3,"preview")
         #-------------------------------------------------------------
